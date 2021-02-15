6cc905d70cc4c4ecad2000d27396abc9b960377b
jiri.andras <jiriandras@protonmail.com>
2021-02-15 22:54:21 +0100

    Add error handling

--- chapter_9_error_handling/src/recovable_errors.rs
+++ chapter_9_error_handling/src/recovable_errors.rs
@@ -1,8 +1,10 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io;
+use std::io::{ErrorKind, Read};
 
 pub fn run() {
     open_file();
+    open_file_again();
 }
 
 fn open_file() {
@@ -18,4 +20,46 @@ fn open_file() {
             other_error => panic!("Problem opening file: {:?}", other_error),
         },
     };
+}
+
+fn open_file_again() {
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating file {:?}", error);
+            })
+        } else {
+            panic!("problem opening file {:?}", error);
+        }
+    });
+}
+
+fn other_handling() {
+    let f = File::open("hello.txt").unwrap();
+    /*
+        returns file if exists or calls panic. Similar to:
+
+    match f {
+        Ok(file)=> file,
+        Err(e)=>panic!()
+    }
+
+    */
+
+    let f = File::open("hello.txt").expect("Some reasonable error message");
+}
+
+fn read_username_from_file(path: &str) -> Result<String, io::Error> {
+    let f = File::open(path);
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
