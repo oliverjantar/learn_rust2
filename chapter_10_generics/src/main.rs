35d3b6f80fc3e26226c0cb64d79a6dd2b609e32a
jiri.andras <jiriandras@protonmail.com>
2021-02-23 20:49:53 +0100

    Foreword of generics

--- chapter_10_generics/src/main.rs
+++ chapter_10_generics/src/main.rs
@@ -6,4 +6,48 @@ fn main() {
     generic_types::run();
     traits::run();
     lifetimes::run();
+
+    // beginning();
+}
+
+fn beginning() {
+    let number_list = vec![54, 2344, 66, 77, 34545, 235621, 5, 22];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("{}", largest);
+
+    let second_largest = largest_number(&number_list);
+    println!("{}", second_largest);
+}
+
+fn largest_number(number_list: &Vec<i32>) -> &i32 {
+    let mut largest_number = &number_list[0];
+
+    for number in number_list {
+        if number > largest_number {
+            largest_number = number;
+        }
+    }
+
+    largest_number
+}
+
+//this is a better implementation as it takes any concrete slice of i32 values
+fn largest(number_list: &[i32]) -> &i32 {
+    let mut largest_number = &number_list[0];
+
+    for number in number_list {
+        if number > largest_number {
+            largest_number = number;
+        }
+    }
+
+    largest_number
 }
