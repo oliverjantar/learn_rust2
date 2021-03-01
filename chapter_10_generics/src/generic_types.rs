93c8f1f0dcdb8863242a29eb9f2cdbd72fc5d54a
jiri.andras <jiriandras@protonmail.com>
2021-03-01 22:55:51 +0100

    Generics wip

--- chapter_10_generics/src/generic_types.rs
+++ chapter_10_generics/src/generic_types.rs
@@ -31,3 +31,54 @@ fn largest_char(list: &[char]) -> &char {
     }
     largest
 }
+
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn make_a_point() {
+    let p = Point { x: 5, y: 10 };
+    let p2 = Point { x: 1.3, y: 2.5 };
+
+    let p3 = Point2 { x: 3.2, y: 32 };
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point2<T, U> {
+    //in this case mixup takes completely different implementation of point other than <T,U>
+    //that's why we need to bring <V,W>
+    fn mixup<V, W>(self, other: Point2<V, W>) -> Point2<T, W> {
+        Point2 {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
