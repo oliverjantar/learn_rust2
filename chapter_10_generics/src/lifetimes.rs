6954b70ece314836a6c06bb76d658a7a1bf5e6c2
jiri.andras <jiriandras@protonmail.com>
2021-04-13 11:40:59 +0200

    Finish lifetimes

--- chapter_10_generics/src/lifetimes.rs
+++ chapter_10_generics/src/lifetimes.rs
@@ -1,1 +1,39 @@
-pub fn run() {}
+pub fn run() {
+    run_part();
+}
+
+fn run_longest_slices() {
+    let string = String::from("This is the longest string of all strings");
+
+    let x = &string[0..5];
+    let y = &string[2..9];
+
+    let result = longest(x, y);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if (x.len() > y.len()) {
+        x
+    } else {
+        y
+    }
+}
+
+fn return_string() -> String {
+    String::from("fdfdf")
+}
+
+struct Part<'a> {
+    text: &'a str,
+}
+
+fn run_part() {
+    let x = String::from("this is an important text");
+
+    let part = Part { text: x.as_str() };
+
+    println!("x is: {}", x);
+    println!("text part is: {}", part.text);
+
+    let static_message: &'static str = "I live forever";
+}
