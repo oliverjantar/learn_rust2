f3e38e843fc231f2f4929ce5eb3e74948c106380
oliverjantar <jiriandras@protonmail.com>
2021-10-24 23:25:46 +0200

    Finished with smart pointers

--- chapter_15_smart_pointers/src/main.rs
+++ chapter_15_smart_pointers/src/main.rs
@@ -2,10 +2,12 @@ mod boxing;
 mod memory_leaks;
 mod ref_cell_example;
 mod reference_counter;
+mod tree_example;
 
 fn main() {
     // boxing::run();
     // reference_counter::run();
     // ref_cell_example::run();
-    memory_leaks::run();
+    // memory_leaks::run();
+    tree_example::run();
 }
