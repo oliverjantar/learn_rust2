a216cd71ee0d79496628eacc688bd9fc16459040
oliverjantar <jiriandras@protonmail.com>
2022-04-23 23:34:04 +0200

    Skip list continue

--- chapter_3_common/src/control_flow.rs
+++ chapter_3_common/src/control_flow.rs
@@ -2,6 +2,7 @@ pub fn run() {
     run_loop();
     run_while();
     loop_collection();
+    run_match_if();
 }
 
 fn run_loop() {
@@ -36,5 +37,14 @@ fn loop_collection() {
 
     for element in (1..4).rev() {
         println!("Reverse loop: element is {}", element);
+    }
+}
+
+fn run_match_if() {
+    let x = Some(5);
+
+    match x {
+        Some(y) if y < 4 => println!("it is less than 4"),
+        _ => println!("it is higher than 4"),
     }
 }
