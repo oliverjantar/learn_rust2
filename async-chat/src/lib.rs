d2368dd084ccec403f8b1e7c2ce8a6e2e5d75e5b
jiriandras <jiriandras@protonmail.com>
2023-04-10 23:14:16 +0200

    Add async-chat, crate structure

---
+++ async-chat/src/lib.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+
+pub mod utils;
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum FromClient {
+    Join {
+        group_name: Arc<String>,
+    },
+    Post {
+        group_name: Arc<String>,
+        message: Arc<String>,
+    },
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+pub enum FromServer {
+    Message {
+        group_name: Arc<String>,
+        message: Arc<String>,
+    },
+    Error(String),
+}
